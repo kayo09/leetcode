ec4d5f49febc5d7df389d3bbb2267ddf6322db1c
Kay Parmar <68217041+kayo09@users.noreply.github.com>
2024-05-25 20:49:08 +0530

    couldn't complete 100314 on time, moving on to 100309

--- 100314/main.py
+++ 100314/main.py
@@ -1,15 +1,21 @@
 class Solution(object):
     numLine=[0]
+    results=[]
     def getResults(self, queries):
+        sz=0
+        obstacle=0
         for row,rowValue in enumerate(queries):
             for column,columnValue in enumerate(queries[row]):
                 if(columnValue==1 and column==0):
-                    blockVal=queries[(columnValue+1)%len(queries[row])]
-                    self.numLine.append(blockVal)
+                    obstacle=queries[(columnValue+1)%len(queries[row])]
+                    self.numLine.append(obstacle)
+                    self.results.append(False)
+                    break
+                sz=queries[row][len(queries[row])-1]
 
-                    print(self.numLine)
-                # print(column,columnValue)
-
+                for num in self.numLine:
+                    print(num)
+            print(sz)
 solution=Solution()
 queries = [[1,2],[2,3,3],[2,3,1],[2,2,2]]
 solution.getResults(queries)
