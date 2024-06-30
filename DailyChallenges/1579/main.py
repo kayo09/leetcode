d8a1bd89b47e5c89f73013d3230512ebf6d853b6
Kay Parmar <68217041+kayo09@users.noreply.github.com>
2024-06-30 22:05:58 +0530

    leetcode is kicking my a**

---
+++ DailyChallenges/1579/main.py
@@ -0,0 +1,15 @@
+#mf broke my streak
+#uses a data structure called union-find
+class Solution(object):
+    def maxNumEdgesToRemove(self, n, edges):
+
+
+
+
+
+
+        
+solution=Solution()
+n=4
+edges=[[3,1,2],[3,2,3],[1,1,3],[1,2,4],[1,1,2],[2,3,4]]
+solution.maxNumEdgesToRemove(n,edges)
