8210250314eaa89f34aa0facc90a1ead6970e624
Kay Parmar <68217041+kayo09@users.noreply.github.com>
2024-07-03 23:35:43 +0530

    broke my 2 day streak

---
+++ DailyChallenges/main.py
@@ -0,0 +1,24 @@
+#mf broke my streak
+'''
+i think the way i might go about solving this would basically be to iterate through the
+nums array and append the diff (maximum - minimum) to a dictionary(keys
+being the difference and values being a nums[i]). now
+i would sort the dictionary in order of increasing differences, 
+
+nvm i am clueless 
+ 
+'''
+class Solution(object):
+    def minDifference(self, nums):
+        maxs='0'
+        maxs=maxs+'a'
+        
+        print(maxs)
+        #for i in range(len(nums)):
+
+
+    
+solution=Solution()
+nums=[5,3,2,4]
+results=solution.minDifference(nums)
+#print(results)
